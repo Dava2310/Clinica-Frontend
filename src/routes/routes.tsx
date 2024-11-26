1e600a13faf822319f2f0dd7529de628e08c7207
mvblanco99 <manuelvalentin1999@gmail.com>
2024-11-26 18:53:21 -0400

    Movimos el componente ProtectedRoute a otro archivo

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,56 +1,18 @@
 import Login from "../components/modules/login";
 import Citas from "../components/modules/doctors/modules/Citas"
-import { getCookie } from "../utils/cookies";
-import { nameCookieSessionApp } from "../config";
 import Main from "../components/main/Main";
 import ViewDoctors from "../components/modules/doctors/modules/ViewDoctors";
 import EditDoctor from "../components/modules/doctors/modules/EditDoctor";
 import TemplateDoctors from "../components/modules/doctors";
-
-import { Navigate, useLocation } from "react-router-dom";
 import TemplatePatients from "../components/modules/patients";
 import EditPatients from "../components/modules/patients/modules/EditPatients";
 import CreateCita from "../components/modules/patients/modules/CreateCita";
-
-import { prefixUrlsTypeUsers, typeUsers } from "../config";
+import ProtectedRoute from "./ProtectedRoute";
 
 const tUser = "paciente";
 
-// Componente que verifica el rol del usuario
-const ProtectedRoute = ({ children, userRole }) => {
-
-  const location = useLocation();
-
-  //Verificamos si tiene token de acceso
-  if(getCookie(nameCookieSessionApp) === undefined){
-    return <Navigate to={"/login"}/>
-  }
-
-  //Extraemos el prefijo de urls que puede acceder el usuario con su rol asignado
-  const correspondingModule = prefixUrlsTypeUsers.filter(e => e.type == userRole)[0];
-
-  //Si tiene sesion activa e intenta redirigir al login redireccionamos al home de su respectivo modulo
-  if(location.pathname.includes('/login')){
-    return <Navigate to={correspondingModule.url}/>;
-  }
-  
-  // Si no cumple con los requerimientos para acceder, se redirige al home del usuario correspondiente
-  if (userRole === typeUsers.doctor && location.pathname.includes(correspondingModule.url)) {
-    return children; // Permite acceso a rutas de doctores
-  } else if (userRole === typeUsers.paciente && location.pathname.includes(correspondingModule.url)) {
-    return children // Redirige a home si es paciente
-  }
-
-  return <Navigate to={correspondingModule.url}/>;
-};
-
 export const routes = [
   {
-    loader: () => {
-      // if(getCookie(nameCookieSessionApp) === undefined){
-      //     throw {statusText: "Unauthenticated",  status: 401 };
-      // }
-  },
     path:'/',
     element:(
       <ProtectedRoute userRole={tUser}>
@@ -68,8 +30,7 @@ export const routes = [
         ),
         children : [
           {
-              path:"citas_pendientes", 
-              element:<Citas/>
+              path:"citas_pendientes",
           },
           {
             path:"modify",
