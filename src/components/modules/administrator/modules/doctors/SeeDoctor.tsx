45e1132a43b0a8024da471849880ca92c9c51a36
Manuel Blanco <manuelb@vitalclinic.local>
2024-12-01 16:38:14 -0400

    Agregamos logica para el renderizado de los datos traidos del servidor

--- src/components/modules/administrator/modules/doctors/SeeDoctor.tsx
+++ src/components/modules/administrator/modules/doctors/SeeDoctor.tsx
@@ -1,9 +1,137 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Table } from "flowbite-react";
+import client from '../../../../../api/client';
 
 const SeeDoctor = () => {
+
+  const [doctors, setDoctors] = useState();
+  const [doctor, setDoctor] = useState();
+  const navigate = useNavigate();
+  const redirecToCreateADoctor = () => navigate('/administrador/crear_doctor')
+  const apiClient = client();
+
+  const fetchDoctors = async () => {
+    try {
+      const res = await apiClient.get('api/users/doctor');
+      if(res.status === 200){
+        setDoctors(res.data.body.data)
+      }
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+
+    const fetch = async() => {
+      await fetchDoctors();
+    }
+
+    fetch();
+
+  },[])
+   
   return (
-    <div>SeeDoctor</div>
+    <div className='flex flex-col gap-y-4 p-4'>
+      
+      {/* Contenedor Button y buscador */}
+      <div className='w-full flex justify-between items-center gap-x-4 border-2 border-gray-300 rounded-md p-2 bg-gray-50'>
+        
+        {/* Button */}
+        <button type="button" onClick={redirecToCreateADoctor} className="w-44 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Registrar doctor</button>
+        
+        {/* Buscador */}
+        <div className='sm:w-full lg:w-[30%]'>
+          <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+          <input 
+            type="text"
+            placeholder='Ingresa un nombre'    
+            className="bg-gray-100 border border-gray-500 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"    
+          />         
+        </div>
+
+      </div>
+
+      {/* Contendor table */}
+      <div className='w-full flex flex-grow border-2 border-gray-300 rounded-md p-4 bg-gray-50'>
+        <div className="w-full  mx-auto">
+          <Table hoverable>
+            
+            <Table.Head>
+              <Table.HeadCell>Nombre</Table.HeadCell>
+              <Table.HeadCell>Email</Table.HeadCell>
+              <Table.HeadCell>Especialidad</Table.HeadCell>
+              <Table.HeadCell>Teléfono</Table.HeadCell>
+              <Table.HeadCell>
+                <span className="sr-only">Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+        
+            <Table.Body className="divide-y">
+
+              {
+                doctors &&
+                  doctors.map(e => {
+                    return <Table.Row key={e.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                      <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                      {e.nombre} {e.apellido}
+                      </Table.Cell>
+                      <Table.Cell>{e.email}</Table.Cell>
+                      <Table.Cell>{e.especialidad}</Table.Cell>
+                      <Table.Cell>{e.numeroTelefono}</Table.Cell>
+                      <Table.Cell className='flex gap-x-2'>
+                      <button type="button" className="w-20  text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Editar</button>
+                      <button type="button" className="w-24  text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Eliminar</button>
+                      </Table.Cell>
+                    </Table.Row>
+                    
+                  })
+              }
+            </Table.Body>
+         </Table>
+        </div>
+      </div>
+    </div>
   )
 }
 
 export default SeeDoctor
+
+/**<Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  {'Apple MacBook Pro 17"'}
+                </Table.Cell>
+                <Table.Cell>Sliver</Table.Cell>
+                <Table.Cell>Laptop</Table.Cell>
+                <Table.Cell>$2999</Table.Cell>
+                <Table.Cell className='flex gap-x-2'>
+                <button type="button" className="w-20  text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Editar</button>
+                <button type="button" className="w-24  text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Eliminar</button>
+                </Table.Cell>
+              </Table.Row>
+
+              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  Microsoft Surface Pro
+                </Table.Cell>
+                <Table.Cell>White</Table.Cell>
+                <Table.Cell>Laptop PC</Table.Cell>
+                <Table.Cell>$1999</Table.Cell>
+                <Table.Cell className='flex gap-x-2'>
+                  <button type="button" className="w-20  text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Editar</button>
+                  <button type="button" className="w-24  text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Eliminar</button>
+                </Table.Cell>
+              </Table.Row>
+
+              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Magic Mouse 2</Table.Cell>
+                <Table.Cell>Black</Table.Cell>
+                <Table.Cell>Accessories</Table.Cell>
+                <Table.Cell>$99</Table.Cell>
+                <Table.Cell className='flex gap-x-2'>
+                  <button type="button" className="w-20  text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Editar</button>
+                  <button type="button" className="w-24  text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Eliminar</button>
+                </Table.Cell>
+              </Table.Row> */
