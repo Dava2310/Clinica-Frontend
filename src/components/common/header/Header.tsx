b8b6209cd5fd6e678f9cfecf4c59a952167f6164
mvblanco99 <manuelvalentin1999@gmail.com>
2024-12-06 22:24:33 -0400

    Implementamos logica para mostrar data del usuario logueado y realizar peticion de cerrado de sesion

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,6 +1,36 @@
 import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import { nameCookieSessionApp } from "../../../config";
+import { getCookie, deleteCookie } from "../../../utils/cookies";
+import { PropsToken } from "../../../types";
+import { useNavigate } from "react-router-dom";
+import client from "../../../api/client";
 
 function Header() {
+  const navigate = useNavigate();
+  const apiClient = client();
+  const token:PropsToken = getCookie(nameCookieSessionApp);
+
+  const logout = async () => {
+
+    console.log('logoutt')
+    try {
+      const res = await apiClient.get('/api/auth/logout'); 
+      console.log(res)
+      if(res.status === 204) {
+        //Eliminamos la cookie
+        deleteCookie(nameCookieSessionApp);
+        //Redireccionamos al login
+        navigate('/login');
+      }
+     } catch (err) {
+       console.log(err)
+       //Redireccionamos por no estar autenticado
+       if(err?.response?.data.statusCode === 401){
+        navigate('/login');
+      }
+     }
+  }
+
   return (
     <Navbar fluid rounded>
       <Navbar.Brand href="https://flowbite-react.com">
@@ -16,14 +46,14 @@ function Header() {
           }
         >
           <Dropdown.Header>
-            <span className="block text-sm">Bonnie Green</span>
-            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+            <span className="block text-sm">{token?.nombre} {token?.apellido}</span>
+            <span className="block truncate text-xs font-medium">{token?.email}</span>
           </Dropdown.Header>
           <Dropdown.Item>Dashboard</Dropdown.Item>
           <Dropdown.Item>Settings</Dropdown.Item>
           <Dropdown.Item>Earnings</Dropdown.Item>
           <Dropdown.Divider />
-          <Dropdown.Item>Sign out</Dropdown.Item>
+          <Dropdown.Item onClick={logout}>Cerrar Sesión</Dropdown.Item>
         </Dropdown>
         {/* <Navbar.Toggle /> */}
       </div>
